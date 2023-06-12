f3c62f170c59bb9e4e2e7bc074ccad21f3f3afa5
Md-arefin <ansariarefin617@gmail.com>
2023-06-12 16:30:08 +0600

    payment history

--- index.js
+++ index.js
@@ -8,25 +8,21 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const stripe = require("stripe")(process.env.STRIPE_KEY)
 
 // middleware
-const corsOptions = {
-  origin: '*',
-  credentials: true,
-  optionSuccessStatus: 200,
-}
-app.use(cors(corsOptions));
+
+app.use(cors());
 app.use(express.json());
 
-const verifyJWT = (req, res, next) =>{
+const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
-  if(!authorization){
-    return res.status(401).send({error: true, message: 'unauthorized access' })
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
   }
 
   const token = authorization.split(' ')[1];
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
-    if(err){
-      return res.status(401).send({error: true, message: 'unauthorized access' })
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
     }
 
     req.decoded = decoded;
@@ -58,20 +54,31 @@ async function run() {
     const paymentCollection = client.db('creativeCaptureDB').collection('payments');
     const userCollection = client.db('creativeCaptureDB').collection('users');
 
-    app.post('/jwt', (req, res) =>{
+    app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user , process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h'})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
 
-      res.send({token})
+      res.send({ token })
     })
 
-    // get popular classes data
+    // verify ADmin 
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      if(user?.role !== 'admin' ){
+        return res.status(403).send({error: true, message: 'forbidden message'})
+      }
+      next();
+    }
 
-    app.get('/popularClass', async (req, res) => {
-      const cursor = classCollection.find().sort({ numberOfStudents: -1 }).limit(6);
-      const result = await cursor.toArray();
-      res.send(result);
-    })
+      // get popular classes data
+
+      app.get('/popularClass', async (req, res) => {
+        const cursor = classCollection.find().sort({ numberOfStudents: -1 }).limit(6);
+        const result = await cursor.toArray();
+        res.send(result);
+      })
 
     // get popular instructors data
 
@@ -105,9 +112,9 @@ async function run() {
       }
 
       const decodedEmail = req.decoded.email;
-    
-      if(email !== decodedEmail){
-        return res.status(403).send({error: true, message: 'forbidden access' })
+
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'forbidden access' })
       }
 
       const query = { email: email };
@@ -132,13 +139,13 @@ async function run() {
 
     //create payments intent ------------------
 
-    app.post('/create-payment-intent', async (req, res) => {
+    app.post('/create-payment-intent', verifyJWT, async (req, res) => {
       const { price } = req.body;
-      const amount = price * 100;
+      const amount = Math.round(price * 100);
       // console.log(amount)
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
-        currency: 'usd',
+        currency: 'USD',
         payment_method_types: ['card']
       })
       res.send({
@@ -146,8 +153,9 @@ async function run() {
       })
     })
 
-    // payment related api
-    app.post('/payments', async (req, res) => {
+    // payment api ------------------------
+
+    app.post('/payments', verifyJWT, async (req, res) => {
       const payment = req.body;
       const InsertResult = await paymentCollection.insertOne(payment);
       const query = { _id: { $in: payment.selectedClassItems.map(id => new ObjectId(id)) } }
@@ -155,9 +163,17 @@ async function run() {
       res.send({ InsertResult, deleteResult });
     })
 
+    app.get('/payments/:email', verifyJWT, async (req, res) =>{
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await paymentCollection.find(query).toArray()
+      res.send(result)
+    })
+
+
     // user api
 
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result)
     })
@@ -175,37 +191,38 @@ async function run() {
     })
 
     //  check admin 
-    app.get('/users/admin/:email',verifyJWT, async (req, res) =>{
+
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
       const email = req.params.email;
 
       const decodedEmail = req.decoded.email;
-    
-      if(email !== decodedEmail){
-        return res.status(403).send({error: true, message: 'forbidden access' })
+
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'forbidden access' })
       }
 
 
       const query = { email: email };
       const user = await userCollection.findOne(query);
-      const result = { admin: user?.role === 'admin'}
+      const result = { admin: user?.role === 'admin' }
       res.send(result);
     })
 
     // check instructor
 
-    app.get('/users/instructor/:email',verifyJWT, async (req, res) =>{
+    app.get('/users/instructor/:email', verifyJWT, async (req, res) => {
       const email = req.params.email;
 
       const decodedEmail = req.decoded.email;
-    
-      if(email !== decodedEmail){
-        return res.status(403).send({error: true, message: 'forbidden access' })
+
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'forbidden access' })
       }
 
 
       const query = { email: email };
       const user = await userCollection.findOne(query);
-      const result = { instructor: user?.role === 'instructor'}
+      const result = { instructor: user?.role === 'instructor' }
       res.send(result);
     })
 
